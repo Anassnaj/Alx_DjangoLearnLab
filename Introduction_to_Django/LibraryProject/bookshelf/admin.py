f392a57f606f5e27b0e93afe77d3df9150a7dc6b
anass nagib <anasnajib19@gmail.com>
2024-11-03 01:13:24 +0100

    Introduction to Django Development Environment Setup

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
-"from  .models import Book"
+from  .models import Book
 
-class BookAdmin(admin.ModelAdmin):
-    list_display =('title', 'author', 'publication_year')
-    list_filter = ('author', 'publication_year')
-    search_fields = ('title', 'author_name')
 
 admin.site.register(Book, BookAdmin)
