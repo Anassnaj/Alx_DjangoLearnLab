59953f58a289aef1e81ad5b1850ec01cf40f2fad
anass nagib <anasnajib19@gmail.com>
2024-11-09 15:19:54 +0100

    Implementing Advanced Model Relationships in Django

---
+++ django-models/Introduction_to_Django/LibraryProject/relationship_app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    # Define choices for roles
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+    
+    # One-to-one relationship with the User model
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    # Role field with choices
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Member')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
