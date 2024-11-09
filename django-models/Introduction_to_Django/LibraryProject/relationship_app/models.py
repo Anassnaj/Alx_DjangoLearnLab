9aa3a573a975c8b1e66c5b085ae51b749b91cc65
anass nagib <anasnajib19@gmail.com>
2024-11-09 15:24:17 +0100

    Implementing Advanced Model Relationships in Django

--- django-models/Introduction_to_Django/LibraryProject/relationship_app/models.py
+++ django-models/Introduction_to_Django/LibraryProject/relationship_app/models.py
@@ -1,19 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-class UserProfile(models.Model):
-    # Define choices for roles
-    ROLE_CHOICES = [
-        ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member'),
-    ]
-    
-    # One-to-one relationship with the User model
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
-    # Role field with choices
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Member')
+class Author(models.Model):
+    name = models.CharField(max_length=255)
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Library(models.Model):
+    name = models.CharField(max_length=255)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=255)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
