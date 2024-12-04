011a661a0d314a9e83370ab0850e515479428554
anass nagib <anasnajib19@gmail.com>
2024-12-04 23:16:15 +0100

    Implementing Advanced Features: Tagging and Search Functionality

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -31,3 +31,14 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+from taggit.managers import TaggableManager
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    tags = TaggableManager()
+
+    def __str__(self):
+        return self.title
