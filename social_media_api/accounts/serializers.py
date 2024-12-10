502c6bf2e0b2c9dcb7216dae5ad0707db48412af
anass nagib <anasnajib19@gmail.com>
2024-12-10 14:44:15 +0100

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 from .models import CustomUser
 from django.contrib.auth.password_validation import validate_password
+from rest_framework.authtoken.models import Token
+
+User = get_user_model().objects.create_user
 
 class UserSerializer(serializers.ModelSerializer):
-    class Meta:
+   password = serializers.CharField(write_only=True, validators=[validate_password])
+ 
+   class Meta:
         model = CustomUser
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
         extra_kwargs = {'password': {'write_only': True}}
